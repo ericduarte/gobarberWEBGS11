96ce9baaa8b2587f4af70bfbe02ce2e41bdcf0bd
Eric Duarte <ericduartemail@gmail.com>
2020-05-20 16:20:09 -0300

    Signup Page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,10 +3,12 @@ import { Switch, Route } from 'react-router-dom';
 
 // import Dashboard from '../pages/Dashboard';
 import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
+    <Route path="/signup" exact component={SignUp} />
   </Switch>
 );
 
